f9e5ab7bf0abb9bba84b8e514f219d2540ddd8ff
German <72597058+GM-GitH@users.noreply.github.com>
2023-04-17 16:33:15 +0200

    feat(context): adds some hardcoded tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
+import { TaskCard } from "@/components/TaskCard";
+import { useTasks } from "@/context/TaskContext";
+import React from "react";
 
 function Page() {
+  const { tasks } = useTasks();
   return (
-    <div>Task Form</div>
-  )
+    <div>
+      {tasks.map(task => (
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
+  );
 }
 
-export default Page
+export default Page;
--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,19 +1,21 @@
-'use client';
+"use client";
 import { createContext, useContext } from "react";
 
-export const TaskContext = createContext<{ tasks: Task[] }>({
-  tasks: [],
-});
+export const TaskContext = createContext<{ tasks: Task[] }>(
+  {
+    tasks: [],
+  }
+);
 
 interface ChildrenProps {
   children: React.ReactNode;
 }
 
 export interface Task {
-  id: string;
-  title: string;
-  description: string;
-  status: boolean;
+  id?: number;
+  title?: string;
+  description?: string;
+  status?: boolean;
 }
 
 interface TasksProps {
@@ -23,13 +25,36 @@ interface TasksProps {
 export const useTasks = () => {
   const context = useContext(TaskContext);
   if (!context) {
-    throw new Error("useTasks must be used within a TaskProvider");
+    throw new Error(
+      "useTasks must be used within a TaskProvider"
+    );
   }
   return context;
 };
 
-export const TaskProvider = ({ children }: ChildrenProps) => {
-  const tasks: Task[] = [];
+export const TaskProvider = ({
+  children,
+}: ChildrenProps) => {
+  const tasks: Task[] = [
+    {
+      id: 0,
+      title: "Task 0",
+      description: "Description 0",
+      status: false,
+    },
+    {
+      id: 1,
+      title: "Task 1",
+      description: "Description 1",
+      status: false,
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      description: "Description 2",
+      status: false,
+    },
+  ];
   return (
     <TaskContext.Provider value={{ tasks }}>
       {children}
